af6abb8b83e1b3b1d054bde02b198bad522cc887
moal2839 <moal2839@colorado.edu>
2021-02-28 01:21:16 +0300

    Unshelving Askar

--- PlanneradoraAPP/app/src/main/java/com/example/myapplication/MainActivity.java
+++ PlanneradoraAPP/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,12 +3,37 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void onClick(View view) {
+
+    }
+
+    public static class Task{
+        String date;
+        int difficulty;
+        boolean finished;
+
+        Task(){
+            this.date = "";
+            this.difficulty = 1;
+            this.finished = false;
+        }
+
+        Task(String date, int difficulty, boolean finished){
+            this.date = date;
+            this.difficulty = difficulty;
+            this.finished = finished;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
 }
