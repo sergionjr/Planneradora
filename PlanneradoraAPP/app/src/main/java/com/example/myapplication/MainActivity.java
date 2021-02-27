4a66c4b14b0af6d23610f6b89e08057fd116a262
paimang <paiman03@gmail.com>
2021-02-27 15:51:43 -0700

    Added Date/Time into front page

--- PlanneradoraAPP/app/src/main/java/com/example/myapplication/MainActivity.java
+++ PlanneradoraAPP/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,12 +3,41 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import java.text.DateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView month,day,year;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        month = findViewById(R.id.month);
+        day = findViewById(R.id.day);
+        year = findViewById(R.id.year);
+
+        Date currentTime = Calendar. getInstance().getTime();
+        String formattedDate = DateFormat.getDateInstance(DateFormat.FULL).format(currentTime);
+
+        String[] splitDate = formattedDate.split(",");
+
+        Log.d("myLOG", currentTime.toString());
+        Log.d("myLOG", formattedDate);
+        month.setText(splitDate[1]);
+        day.setText(splitDate[0]);
+        year.setText(splitDate[2]);
+        Log.d("myLOG", splitDate[0].trim());
+        Log.d("myLOG", splitDate[1].trim());
+        Log.d("myLOG", splitDate[2].trim());
+
+
     }
 }
