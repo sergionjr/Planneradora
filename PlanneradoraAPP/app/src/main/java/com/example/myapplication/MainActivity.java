578cda52a40b03daf5312ab8efa0c0771bf701f1
Sergio <seno7509@colorado.edu>
2021-03-02 14:59:41 -0700

    Sergio - switching

--- PlanneradoraAPP/app/src/main/java/com/example/myapplication/MainActivity.java
+++ PlanneradoraAPP/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -86,8 +86,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-  //      setContentView(R.layout.activity_main);
-        setContentView(R.layout.registration_page);
+        setContentView(R.layout.activity_main);
 
         btn_signIn = findViewById(R.id.btn_signIn);
 
