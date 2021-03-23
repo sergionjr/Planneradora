aefec5676fabddd598b84d7e15bcee1e84ed6594
paimang <paiman03@gmail.com>
2021-03-23 16:26:38 -0600

    added button to pick date.

--- PlanneradoraAPP/app/src/main/java/com/example/myapplication/AddTaskActivity.java
+++ PlanneradoraAPP/app/src/main/java/com/example/myapplication/AddTaskActivity.java
@@ -1,13 +1,17 @@
 package com.example.myapplication;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Calendar;
 
 public class AddTaskActivity extends AppCompatActivity implements View.OnClickListener{
 
@@ -15,11 +19,40 @@ public class AddTaskActivity extends AppCompatActivity implements View.OnClickLi
     TextView choose_diff;
     public static EditText task_name, task_date, task_description;
     public static MainActivity.Task t = new MainActivity.Task();
+    TextView mTv;
+    Button mBtn;
+
+    Calendar c;
+    DatePickerDialog dpd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_task);
+
+        mTv = (TextView) findViewById(R.id.txtView);
+        mBtn = (Button) findViewById(R.id.btnPick);
+
+        mBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+
+                c = Calendar.getInstance();
+                int day = c.get(Calendar.DAY_OF_MONTH);
+                int month = c.get(Calendar.MONTH);
+                int year = c.get(Calendar.YEAR);
+
+                dpd = new DatePickerDialog(AddTaskActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int mYear, int mMonth, int mDay) {
+                        mTv.setText(mDay + "/" + (mMonth+1) + "/" +mYear);
+                    }
+                }, day, month, year);
+                dpd.show();
+
+            }
+        });
+
 //
 //        diff1 = (Button) findViewById(R.id.btnDiff1);
 //        diff2 = (Button) findViewById(R.id.btnDiff2);
